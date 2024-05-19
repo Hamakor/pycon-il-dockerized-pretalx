9f459efdba07273b75600b6710cb06e0f8d1a3f0
Shai Berger <shai@platonix.com>
2024-05-19 23:33:41 +0300

    Add Hebrew support to vendored calendar.io
    Fixes error when submitting in Hebrew

---
+++ pretalx/calendar.io/he.js
@@ -0,0 +1,9 @@
+/*!
+FullCalendar Core v6.1.5
+Docs & License: https://fullcalendar.io
+(c) 2023 Adam Shaw
+
+(content copied on 2024-05-19 from
+https://github.com/fullcalendar/fullcalendar/blob/main/packages/core/src/locales/he.ts )
+*/
+!function(e){"use strict";FullCalendar.globalLocales.push({code: 'he', direction: 'rtl', buttonText: { prev: 'הקודם', next: 'הבא', today: 'היום', year: 'שנה', month: 'חודש', week: 'שבוע', day: 'יום', list: 'סדר יום', }, allDayText: 'כל היום', moreLinkText: 'נוספים', noEventsText: 'אין אירועים להצגה', weekText: 'שבוע', })}();
